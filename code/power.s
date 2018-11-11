b6a9f4f4b11baecf296ba0903d5dcbafe1312e61
Joren Van Onder <joren.vanonder@gmail.com>
2018-11-11 12:56:15 -0800

    Convert back to 32-bit assembly from book

--- code/power.s
+++ code/power.s
@@ -9,24 +9,24 @@
 .globl _start
 
 _start:
-        push $3                 #push second argument
-        push $2                 #push first argument
-        call power              #call the function
-        add  $8, %rsp           #move the stack pointer back
+        pushl $3                #push second argument
+        pushl $2                #push first argument
+        call  power             #call the function
+        addl  $8, %esp          #move the stack pointer back
 
-        push %rax               #save the first answer before calling the next function
-        push $2                 #push second argument
-        push $5                 #push first argument
-        call power              #call the function
-        add  $8, %rsp           #move the stack pointer back
-        pop  %rbx               #The second answer is already
-                                #in %rax.  We saved the
+        pushl %eax              #save the first answer before calling the next function
+        pushl $2                #push second argument
+        pushl $5                #push first argument
+        call  power             #call the function
+        addl  $8, %esp          #move the stack pointer back
+        popl  %ebx              #The second answer is already
+                                #in %eax.  We saved the
                                 #first answer onto the stack,
                                 #so now we can just pop it
-                                #out into %rbx
-        add   %rax, %rbx        #add them together
-                                #the result is in %rbx 
-        mov   $1, %rax          #exit (%rbx is returned)
+                                #out into %ebx
+        addl  %eax, %ebx        #add them together
+                                #the result is in %ebx
+        movl  $1, %eax          #exit (%ebx is returned)
         int   $0x80
 
 #PURPOSE:  This function is used to compute
@@ -42,30 +42,30 @@ _start:
 #NOTES:    The power must be 1 or greater
 #
 #VARIABLES:
-#          %rbx - holds the base number
-#          %rcx - holds the power
-#          -4(%rbp) - holds the current result
-#          %rax is used for temporary storage
+#          %ebx - holds the base number
+#          %ecx - holds the power
+#          -4(%ebp) - holds the current result
+#          %eax is used for temporary storage
 #
 .type power, @function
 power:
-        push  %rbp              #save old base pointer
-        mov  %rsp, %rbp         #make stack pointer the base pointer
-        sub  $4, %rsp           #get room for our loca storage
-        mov  8(%rbp), %rbx      #put first argument in %rax
-        mov  12(%rbp), %rcx     #put second argument in %rcx
-        mov  %rbx, -4(%rbp)     #store current result
+        pushl %ebp              #save old base pointer
+        movl  %esp, %ebp        #make stack pointer the base pointer
+        subl  $4, %esp          #get room for our local storage
+        movl  8(%ebp), %ebx     #put first argument in %eax
+        movl  12(%ebp), %ecx    #put second argument in %ecx
+        movl  %ebx, -4(%ebp)    #store current result
 power_loop_start:
-        cmp  $1, %rcx           #if the power is 1, we are done
+        cmpl  $1, %ecx          #if the power is 1, we are done
         je    end_power
-        mov  -4(%rbp), %rax     #move the current result into %rax
-        imul %rbx, %rax         #multiply the current result by
+        movl  -4(%ebp), %eax    #move the current result into %eax
+        imull %ebx, %eax        #multiply the current result by
                                 #the base number
-        mov  %rax, -4(%rbp)     #store the current result
-        dec  %rcx               #decrease the power
+        movl  %eax, -4(%ebp)    #store the current result
+        decl  %ecx              #decrease the power
         jmp   power_loop_start  #run for the next power
 end_power:
-        mov -4(%rbp), %rax      #return value goes in %rax
-        mov %rbp, %rsp          #restore the stack pointer
-        pop %rbp                #restore the base pointer
+        movl -4(%ebp), %eax     #return value goes in %eax
+        movl %ebp, %esp         #restore the stack pointer
+        popl %ebp               #restore the base pointer
         ret
