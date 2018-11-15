54d6f981df19d4da59762c4b3002e858841e5fa1
Joren Van Onder <joren.vanonder@gmail.com>
2018-11-14 16:55:37 -0800

    Add reader

---
+++ code/records/2_reader.s
@@ -0,0 +1,45 @@
+.include "linux.s"
+.include "record_def.s"
+.include "common.s"
+
+.section .bss
+        .lcomm  RECORD_DATA, RECORD_SIZE
+
+.section .text
+.equ ST_FD, -4
+.equ O_RDONLY, 00000000
+.equ PERMISSIONS, 0644
+.globl _start
+_start:
+        movl    %esp, %ebp
+        subl    $4, %esp                # make room for ST_FD
+
+        ## open file specified in first argument
+        movl    $SYS_OPEN, %eax
+        movl    ST_ARGV_1(%ebp), %ebx
+        movl    $O_RDONLY, %ecx
+        movl    $PERMISSIONS, %edx
+
+        int     $LINUX_SYSCALL
+        movl    %eax, ST_FD(%ebp)       # store fd
+
+        pushl   $RECORD_DATA
+        pushl   ST_FD(%ebp)
+
+        call    read_record
+
+        addl    $8, %esp
+
+print:
+        movl    $SYS_WRITE, %eax
+        movl    $STDOUT, %ebx
+        movl    $RECORD_DATA, %ecx
+        movl    $RECORD_SIZE, %edx
+
+        int     $LINUX_SYSCALL
+
+exit:
+        movl    $SYS_EXIT, %eax                         # prepare exit
+        movl    $0, %ebx                                # return 0
+        int     $0x80                                   # execute syscall
+
--- code/records/build.sh
+++ code/records/build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-as --gstabs --32 1_writer.s -o out.o
-ld -m elf_i386 out.o
+set -euo pipefail
+
+compile () {
+    NAME="${1%.s}"
+
+    as --gstabs --32 "${NAME}.s" -o "${NAME}.o"
+    ld -m elf_i386 "${NAME}.o" -o "${NAME}"
+}
+
+compile '1_writer.s'
+compile '2_reader.s'
